409829e3fee17175570d528da4552418af87fb3f
cmd <DeComrade@proton.me>
2026-09-23 18:54:39 -0500

    Add comments to main.c for clarity

--- main.c
+++ main.c
@@ -5,13 +5,13 @@ int main(int argc, char *argv[]){
 
 	int objectNumber[8]; //initialize the SCP ID input
 	char SCPID[10];	//initialze the string version of the ID
-	char URL[256] = "xdg-open https://scp-wiki.wikidot.com/scp-";
-	char fullURL[10];
+	char URL[256] = "xdg-open https://scp-wiki.wikidot.com/scp-"; //basic version of the command that opens the URL
+	char fullURL[10]; //initialize what will the the full URL
 	
 
-	sprintf(SCPID,"%s", argv[1]);
+	sprintf(SCPID,"%s", argv[1]); //assign the console argument to SCPID
 
-	strcat(URL, SCPID);
+	strcat(URL, SCPID); //finish the command by adding the SCP ID
 
-	system(URL);
+	system(URL); // call the finished command
 }
